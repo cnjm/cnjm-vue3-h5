e3c7f81a46876ee081ba6808bb10e778d0969242
cnjm <cnjm.top>
2024-06-24 11:40:11 +0800

    chore: volar

---
+++ src/views/demo/listHooks/detail/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  defineOptions({ name: "DemoListHooksDetailPage", inheritAttrs: false, meta: { title: "详情", keepAlive: true } });
+</script>
+
+<template>
+  <div>详情</div>
+</template>
+
+<style scoped lang="less"></style>
